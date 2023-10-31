fd7461626e3c9069cd7a5aa9799a9d78b9363077
Wesley Marcos <wesley.marcos@gec.inatel.br>
2023-10-31 16:16:56 -0300

    feat: códigos do curso 'Exceptions'

---
+++ Java_Exceptions/java_exceptions/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Ini do main");
+        metodo1();
+        System.out.println("Fim do main");
+    }
+
+    private static void metodo1() {
+        System.out.println("Ini do metodo1");
+        metodo2();
+        System.out.println("Fim do metodo1");
+    }
+
+    private static void metodo2() {
+        System.out.println("Ini do metodo2");
+        throw new MinhaExcecaoUnchecked("AOOOOOOOOBA");
+        //System.out.println("Fim do metodo2");
+    }
+}
---
+++ Java_Exceptions/java_exceptions/src/MinhaExcecaoChecked.java
@@ -0,0 +1,6 @@
+public class MinhaExcecaoChecked extends Exception{
+
+    public MinhaExcecaoChecked(String msg){
+        super(msg);
+    }
+}
